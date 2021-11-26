dd6301d98669006da747ebc6517abd8af1b58b98
hiterraide <73894784+hiterraide@users.noreply.github.com>
2021-11-26 23:02:49 +0200

    Update 7.py

--- exam/7.py
+++ exam/7.py
@@ -1,7 +1,6 @@
 lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
 for i in lst:
-    if lst%3:
+    if i%3==0 and i<30:
         print(i)
-    elif lst<30:
-        print(i)
-
+numbers = lst[0] + lst[1] + lst[2] + lst[3] + lst[6] + lst[7] + lst[9] + lst[10] + lst[12]
+print(numbers)
